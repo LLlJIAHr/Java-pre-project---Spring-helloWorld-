8e111a2117d381f53d12c8f163270a128b5993d8
LLI.JI.@.H.r <typrostosyn@gmail.com>
2023-01-11 20:30:46 +0700

    Spring task1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloBean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloBean1 == helloBean2);
+
+
+        Cat catBean1 =
+                applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 =
+                applicationContext.getBean("cat", Cat.class);
+        System.out.println(catBean1 == catBean2);
     }
 }
